e44533f36d2542822dc1138595b68c099520b685
Thomas C <thomas.carbillet@gmail.com>
2020-03-20 15:37:36 +0100

    prune(): remove unused IpAddressAdmin

--- django_ip_access/admin.py
+++ django_ip_access/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin, messages
 
 from .forms import EditIpAddressForm
-from .models import EditIpAddress, IpAddress
+from .models import EditIpAddress
 
 
 class EditIpAddressAdmin(admin.ModelAdmin):
@@ -14,13 +14,4 @@ class EditIpAddressAdmin(admin.ModelAdmin):
     form = EditIpAddressForm
 
 
-class IpAddressAdmin(admin.ModelAdmin):
-    raw_id_fields = ("user",)
-    list_select_related = ("user",)
-    list_display = ("ip", "user")
-    ordering = ("user__email",)
-    search_fields = ("ip", "user__email")
-    
-
 admin.site.register(EditIpAddress, EditIpAddressAdmin)
-admin.site.register(IpAddress, IpAddressAdmin)
