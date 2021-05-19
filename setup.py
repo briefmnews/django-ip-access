d6b51310d3c4ccc9557cb205e5b156e9d8a55bcf
Thomas <thomas.carbillet@gmail.com>
2021-05-19 15:32:39 +0200

    feat(django-ip-access): release version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="django-ip-access",
-    version="0.1.5",
+    version="1.0.0",
     description="Access a Django app with authorized IP address",
     url="https://github.com/briefmnews/django-ip-access",
     author="Brief.me",
