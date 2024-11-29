5a2752d47b2733027111bc45075ef14f7f21ce8d
Thomas Lenovo T14 <tcarbillet@brief.me>
2024-11-29 15:28:13 +0100

    feat: add caching mechanism to prevent redundant database queries for the same IP

--- django_ip_access/middleware.py
+++ django_ip_access/middleware.py
@@ -1,10 +1,10 @@
+from django.conf import settings
 from django.contrib.auth import authenticate, login
+from django.core.cache import cache
 from ipware import get_client_ip
 
 
 class IpAccessMiddleware:
-    """Middleware that allows Ip address authentication"""
-
     def __init__(self, get_response):
         self.get_response = get_response
 
@@ -13,9 +13,15 @@ class IpAccessMiddleware:
             return self.get_response(request)
 
         ip, is_routable = get_client_ip(request)
-        user = authenticate(request, ip=ip)
+        cache_key = f"{settings.IP_ACCESS_CACHE_KEY_PREFIX}_{ip}"
 
-        if user:
-            login(request, user, backend="django_ip_access.backends.IpAccessBackend")
+        if not cache.get(cache_key):
+            user = authenticate(request, ip=ip)
+            cache.set(cache_key, True, settings.IP_ACCESS_CACHE_TTL)
+
+            if user:
+                login(
+                    request, user, backend="django_ip_access.backends.IpAccessBackend"
+                )
 
         return self.get_response(request)
